b72572cda23e9ac697f02dea16589f2d5f35383c
stephlerner <47003232+stephlerner@users.noreply.github.com>
2019-04-16 10:22:05 -0500

    Class examples with JS
    Added more examples with JS

--- JavaScript examples w12/js_marquis/js/script.js
+++ JavaScript examples w12/js_marquis/js/script.js
@@ -4,20 +4,30 @@ var allParagraphs = document.getElementsByTagName("p");
 
 var firstParagraph = allParagraphs[0];
 
+function revealCopy() {
+  e.preventDefault();
+}
+
 newLink.setAttribute("href", "#");
 
 newLink.setAttribute("class", "more-link");
 
 newLink.innerHTML="Read More";
 
+newLink.addEventListener("click", revealCopy);
+
 firstParagraph.appendChild( newLink );
 
+fucntion revealCopy(e){
+  var allParagraphs = document.getElementsByTagName("p")
+}
 for (var i=0; i < allParagraphs.length; i++ ){
   if(i===0) {
     continue;
   }
   allParagraphs[i].style.display="none";
 }
+
 
 //newLink.style.display = "inline-block";
 
